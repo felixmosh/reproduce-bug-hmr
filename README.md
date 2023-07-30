0c1e3345d059d1815b336b7ba78f9661309b5502
Tommy Leong <tleong@zuelligpharma.com>
2023-07-30 14:48:16 +0800

    HMR

---
+++ README.md
@@ -0,0 +1,12 @@
+## Frontend
+
+- npm install
+- npm run dev
+- visit localhost:3000
+
+## Backend (LOCAL)
+
+- Refer to this repo (https://github.com/i18next/i18next-multiload-backend-adapter)
+- npm install
+- cd example
+- nodemon app.js
---
+++ next-i18next.config.js
@@ -0,0 +1,27 @@
+const HttpBackend = require('i18next-http-backend/cjs')
+const MultiLoadBackendAdapter = require('i18next-multiload-backend-adapter/cjs')
+const HMRPlugin = require('i18next-hmr/plugin').HMRPlugin;
+
+/** @type {import('next-i18next').UserConfig} */
+module.exports = {
+    i18n: {
+      defaultLocale: 'en',
+      locales: ['en','de']
+    },
+    defaultNS: 'ns1',
+    fallbackNS: 'common',
+    serializeConfig: false,
+
+    // ???
+    use: [MultiLoadBackendAdapter, new HMRPlugin({ client: true })],
+    // use: [MultiLoadBackendAdapter],
+
+    backend: {
+      backend: HttpBackend,
+      backendOption: {
+        expirationTime: 5 * 1000,
+        loadPath: 'http://localhost:8080/locales?lng={{lng}}&ns={{ns}}',
+      }
+    },
+    debug: true,
+  }
